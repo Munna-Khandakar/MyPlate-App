cb5d0d534816563a52564244a95dba0c98f5247a
Munna-Khandakar <munnashisad@gmail.com>
2022-02-11 11:37:17 +0600

    back button handler override

--- App.js
+++ App.js
@@ -1,20 +1,80 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useRef, useState, useCallback, useEffect } from "react";
+import { BackHandler } from "react-native";
+import { StyleSheet, View, Text, Image, StatusBar } from 'react-native';
+import { WebView } from 'react-native-webview';
+import { useNetInfo } from "@react-native-community/netinfo";
+
 
 export default function App() {
-  return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
-  );
-}
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+
+  //@override back button action...
+  const webView = useRef();
+
+  const [canGoBack, setCanGoBack] = useState(false);
+
+  const handleBack = useCallback(() => {
+    if (canGoBack && webView.current) {
+      webView.current.goBack();
+      return true;
+    }
+    return false;
+  }, [canGoBack]);
+
+  useEffect(() => {
+    BackHandler.addEventListener("hardwareBackPress", handleBack);
+    return () => {
+      BackHandler.removeEventListener("hardwareBackPress", handleBack);
+    };
+  }, [handleBack]);
+
+  const netInfo = useNetInfo();
+  const styles = StyleSheet.create({
+    image: { //style for centering the image
+      resizeMode: 'contain',
+      alignSelf: 'center',
+      width: '100%',
+      height: '100%',
+    }
+  })
+
+  if (netInfo.isInternetReachable) { //checking the internet cionnection
+    return (<>
+      <StatusBar
+        backgroundColor="#372b2b"
+        barStyle="light-content"
+        hidden={false}
+        translucent={true}
+      />
+      <WebView  //showing the myplate website
+        ref={webView}
+        startInLoadingState={true}
+        renderLoading={() => <>
+          <View>
+            <Image //showing initial loading  gif
+              source={require('./assets/progess-bar2.gif')}
+              style={styles.image}
+            />
+          </View>
+        </>}
+        pullToRefreshEnabled={true}
+        source={{ uri: 'https://myplate.xyz/' }}
+        style={{ marginTop: 15 }}
+        onLoadProgress={(event) => setCanGoBack(event.nativeEvent.canGoBack)}
+        domStorageEnabled={true}
+        startInLoadingState={true}
+      />
+    </>
+    );
+  } else {  //no internet connection
+    return (
+      <View>
+        <Image //showing no internet gif
+          source={require('./assets/no_internet.gif')}
+          style={styles.image}
+        />
+      </View>
+    )
+  }
+
+};
---
+++ components/webviewComponent.js
@@ -0,0 +1,11 @@
+
+import { StyleSheet, Text, View } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+export default function webviewComponent() {
+  return (
+      <WebView
+        source={{ uri: 'https://myplate.xyz/' }}
+      />
+  );
+};
