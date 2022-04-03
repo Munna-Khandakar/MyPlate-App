77b6eccfb60ad30f3668a37feaa71696ddc4d833
Munna-Khandakar <munnashisad@gmail.com>
2022-04-03 10:20:36 +0600

    App.js updated

--- App.js
+++ App.js
@@ -1,13 +1,10 @@
 import React, { useRef, useState, useCallback, useEffect } from "react";
 import { BackHandler } from "react-native";
-import { StyleSheet, View, Text, Image, StatusBar } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StyleSheet, View, Text, Image, StatusBar } from "react-native";
+import { WebView } from "react-native-webview";
 import { useNetInfo } from "@react-native-community/netinfo";
 
-
 export default function App() {
-
-
   //@override back button action...
   const webView = useRef();
 
@@ -30,51 +27,56 @@ export default function App() {
 
   const netInfo = useNetInfo();
   const styles = StyleSheet.create({
-    image: { //style for centering the image
-      resizeMode: 'contain',
-      alignSelf: 'center',
-      width: '100%',
-      height: '100%',
-    }
-  })
+    image: {
+      //style for centering the image
+      resizeMode: "contain",
+      alignSelf: "center",
+      width: "100%",
+      height: "100%",
+    },
+  });
 
-  if (netInfo.isInternetReachable) { //checking the internet cionnection
-    return (<>
-      <StatusBar
-        backgroundColor="#372b2b"
-        barStyle="light-content"
-        hidden={false}
-        translucent={true}
-      />
-      <WebView  //showing the myplate website
-        ref={webView}
-        startInLoadingState={true}
-        renderLoading={() => <>
-          <View>
-            <Image //showing initial loading  gif
-              source={require('./assets/progess-bar2.gif')}
-              style={styles.image}
-            />
-          </View>
-        </>}
-        pullToRefreshEnabled={true}
-        source={{ uri: 'https://www.myplate.xyz/' }}
-        style={{ marginTop: 15 }}
-        onLoadProgress={(event) => setCanGoBack(event.nativeEvent.canGoBack)}
-        domStorageEnabled={true}
-        startInLoadingState={true}
-      />
-    </>
+  if (netInfo.isInternetReachable) {
+    //checking the internet cionnection
+    return (
+      <>
+        <StatusBar
+          backgroundColor="#372b2b"
+          barStyle="light-content"
+          hidden={false}
+          translucent={true}
+        />
+        <WebView //showing the myplate website
+          ref={webView}
+          startInLoadingState={true}
+          renderLoading={() => (
+            <>
+              <View>
+                <Image //showing initial loading  gif
+                  source={require("./assets/progess-bar2.gif")}
+                  style={styles.image}
+                />
+              </View>
+            </>
+          )}
+          pullToRefreshEnabled={true}
+          source={{ uri: "https://www.myplate.xyz/" }}
+          style={{ marginTop: 15 }}
+          onLoadProgress={(event) => setCanGoBack(event.nativeEvent.canGoBack)}
+          domStorageEnabled={true}
+          //startInLoadingState={true}
+        />
+      </>
     );
-  } else {  //no internet connection
+  } else {
+    //no internet connection
     return (
       <View>
         <Image //showing no internet gif
-          source={require('./assets/no_internet.gif')}
+          source={require("./assets/no_internet.gif")}
           style={styles.image}
         />
       </View>
-    )
+    );
   }
-
-};
+}
--- components/webviewComponent.js
+++ components/webviewComponent.js
@@ -1,11 +1,6 @@
-
-import { StyleSheet, Text, View } from 'react-native';
-import { WebView } from 'react-native-webview';
+import { StyleSheet, Text, View } from "react-native";
+import { WebView } from "react-native-webview";
 
 export default function webviewComponent() {
-  return (
-      <WebView
-        source={{ uri: 'https://myplate.xyz/' }}
-      />
-  );
-};
+  return <WebView source={{ uri: "https://www.myplate.xyz/" }} />;
+}
